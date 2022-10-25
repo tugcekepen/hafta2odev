054f7058f4d2611920906c7fc72d1ab318e5a021
Tugce Kepen <93393198+tugcekepen@users.noreply.github.com>
2022-10-25 19:38:35 +0300

    Add files via upload

---
+++ Employee.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hafta2odev
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string FullName { get; set; }
+
+        public List<Employee> employees;
+
+
+        public Employee()
+        {
+            employees = new List<Employee>();
+        }
+
+        public Employee(int id, string fullName)
+        {
+            this.Id = id;
+            this.FullName = fullName;
+            employees = new List<Employee>();
+
+        }
+
+        public void Add(Employee emp)
+        {
+            employees.Add(emp);
+        }
+
+        public List<Employee> GetEmployees()
+        {
+            return employees;
+        }
+
+        public void Remove(int id)
+        {
+            var emp = employees
+                .Where(i => i.Id == id)
+                .FirstOrDefault();
+
+            if (emp==null)
+            {
+                Console.WriteLine("Silinmek istenen çalışan bulunamadı.");
+            }
+            else
+            {
+                employees.Remove(emp);
+            }
+        }
+
+        public override string? ToString()
+        {
+            return $"{Id, -10} {FullName, 20}";
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using hafta2odev;
+
+var employee = new Employee();
+employee.Add(new Employee(1, "Tuğçe Kepen"));
+employee.Add(new Employee(2, "Alper Başal"));
+employee.Add(new Employee(3, "İrem Kepen"));
+
+employee.Remove(2);
+
+var list = employee.GetEmployees();
+Console.WriteLine("Çalışan Listesi");
+foreach (var item in list)
+{
+    Console.WriteLine("Çalışan Id: " + item.Id + "   Çalışan ad-soyad: " + item.FullName);
+}
